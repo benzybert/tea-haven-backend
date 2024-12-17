5925b52da75c0d0c8f40197633849e53c8b43657
ben roy zybert <96917127+benzybert@users.noreply.github.com>
2024-12-17 14:03:15 +0200

    Add main configuration files

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const { port } = require('./config/config');
+const errorHandler = require('./middleware/errorHandler');
+
+const app = express();
+
+// Middleware to parse incoming requests
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Routes
+const teaRoutes = require('./routes/teaRoutes');
+app.use('/api/teas', teaRoutes);
+
+// Error Handler (should be last)
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
