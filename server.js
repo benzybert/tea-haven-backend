f3e677d626031e73f11df1ce2527e1884d6c10d6
ben roy zybert <96917127+benzybert@users.noreply.github.com>
2024-12-19 13:26:09 +0200

    Force server to use port 5001

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-const PORT = process.env.PORT || 5001;
+// Explicitly use port 5001
+const PORT = 5001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
